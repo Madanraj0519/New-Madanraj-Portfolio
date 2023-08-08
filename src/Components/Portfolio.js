8d1f87d6038596d7bea1429087a70f13f8ae38af
Madan raj <125730836+Madanraj0519@users.noreply.github.com>
2023-08-08 22:53:36 +0530

    changes in portfolio

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -97,53 +97,58 @@ const Portfolio = () => {
                 </div>
             </div>
 
-            {/* E-commerce-website */}
-            <div class={`work rounded-lg overflow-hidden border border-zinc-100 p-2 py-24 hover:relative`}>
-                {/* <h1 className='relative -top-16 bg-red-700 w-24 h-8  py-1 px-3 rounded'>Project</h1> */}
-                <img src={ecommerece} alt="" className='w-full min-h-full  rounded-lg block '/>
-                <div class={`layer w-full h-0 bg-red-700 rounded-lg ${!isShowMode ? "absolute":""} left-0 bottom-0 overflow-hidden flex items-center justify-center flex-col py-o px-10 text-center text-lg`}>
-                    <h2 className='mb-4 font-semibold'>E-commerce website with redux toolkit</h2>
-                    <p>Developed an e-commerce website using Redux Toolkit for state management, showcasing proficiency in API
-                        integration and async thunk for data fetching</p>
-                    <div className='flex gap-3'>
-                    <a className='mt-8 text-black text-lg bg-white w-16 h-16 rounded-xl text-center p-5 hover:scale-110 duration-200 cursor-pointer' onClick={() => OnSlider(Ecommerce)}><GoCodeReview className='text-2xl' /></a>
-                    <a className='mt-8 text-black text-lg bg-white w-16 h-16 rounded-xl text-center p-5 hover:scale-110 duration-200' href="https://e-commerce-website-phi-seven.vercel.app/"><FaLink className='text-2xl' /></a>
-                    <a className='mt-8 text-black text-lg bg-white w-16 h-16 rounded-xl text-center p-5 hover:scale-110 duration-200' href="https://github.com/Madanraj0519/e-commerce-website-redux"><FaGithub className='text-2xl' /></a>
-                    </div>
+           {
+             showMore && (
+              <>
+        <div class={`work rounded-lg overflow-hidden border border-zinc-100 p-2 py-24 hover:relative`}>
+            {/* <h1 className='relative -top-16 bg-red-700 w-24 h-8  py-1 px-3 rounded'>Project</h1> */}
+            <img src={ecommerece} alt="" className='w-full min-h-full  rounded-lg block '/>
+            <div class={`layer w-full h-0 bg-red-700 rounded-lg ${!isShowMode ? "absolute":""} left-0 bottom-0 overflow-hidden flex items-center justify-center flex-col py-o px-10 text-center text-lg`}>
+                <h2 className='mb-4 font-semibold'>E-commerce website with redux toolkit</h2>
+                <p>Developed an e-commerce website using Redux Toolkit for state management, showcasing proficiency in API
+                    integration and async thunk for data fetching</p>
+                <div className='flex gap-3'>
+                <a className='mt-8 text-black text-lg bg-white w-16 h-16 rounded-xl text-center p-5 hover:scale-110 duration-200 cursor-pointer' onClick={() => OnSlider(Ecommerce)}><GoCodeReview className='text-2xl' /></a>
+                <a className='mt-8 text-black text-lg bg-white w-16 h-16 rounded-xl text-center p-5 hover:scale-110 duration-200' href="https://e-commerce-website-phi-seven.vercel.app/"><FaLink className='text-2xl' /></a>
+                <a className='mt-8 text-black text-lg bg-white w-16 h-16 rounded-xl text-center p-5 hover:scale-110 duration-200' href="https://github.com/Madanraj0519/e-commerce-website-redux"><FaGithub className='text-2xl' /></a>
                 </div>
             </div>
+        </div>
 
-            {/* Book-search-app */}
-            <div class={`work rounded-lg overflow-hidden border border-zinc-100 p-2 py-24 hover:relative`}>
-                {/* <h1 className='relative -top-16 bg-red-700 w-24 h-8  py-1 px-3 rounded'>Project</h1> */}
-                <img src={Bookapp} alt="" className='w-full min-h-full  rounded-lg block '/>
-                <div class={`layer w-full h-0 bg-red-700 rounded-lg ${!isShowMode ? "absolute":""} left-0 bottom-0 overflow-hidden flex items-center justify-center flex-col py-o px-10 text-center text-lg`}>
-                    <h2 className='mb-2 md:mb-4 font-semibold'>Book search app</h2>
-                    <p>Developed a book search application using React.js, utilizing API integration to fetch and display real-time book data.</p>
-                    <p className='hidden md:block'>Designed an intuitive user interface allowing users to search for books by title.</p>
-                    <div className='flex gap-3'>
-                    <a className='mt-8 text-black text-lg bg-white w-16 h-16 rounded-xl text-center p-5 hover:scale-110 duration-200 cursor-pointer' onClick={() => OnSlider(Books)}><GoCodeReview className='text-2xl' /></a>
-                    <a className='mt-8 text-black text-lg bg-white w-16 h-16 rounded-xl text-center p-5 hover:scale-110 duration-200' href="https://book-search-app-react-amber.vercel.app/"><FaLink className='text-2xl' /></a>
-                    <a className='mt-8 text-black text-lg bg-white w-16 h-16 rounded-xl text-center p-5 hover:scale-110 duration-200' href="https://github.com/Madanraj0519/Book-search-app-react"><FaGithub className='text-2xl' /></a>
-                    </div>
+        {/* Book-search-app */}
+        <div class={`work rounded-lg overflow-hidden border border-zinc-100 p-2 py-24 hover:relative`}>
+            {/* <h1 className='relative -top-16 bg-red-700 w-24 h-8  py-1 px-3 rounded'>Project</h1> */}
+            <img src={Bookapp} alt="" className='w-full min-h-full  rounded-lg block '/>
+            <div class={`layer w-full h-0 bg-red-700 rounded-lg ${!isShowMode ? "absolute":""} left-0 bottom-0 overflow-hidden flex items-center justify-center flex-col py-o px-10 text-center text-lg`}>
+                <h2 className='mb-2 md:mb-4 font-semibold'>Book search app</h2>
+                <p>Developed a book search application using React.js, utilizing API integration to fetch and display real-time book data.</p>
+                <p className='hidden md:block'>Designed an intuitive user interface allowing users to search for books by title.</p>
+                <div className='flex gap-3'>
+                <a className='mt-8 text-black text-lg bg-white w-16 h-16 rounded-xl text-center p-5 hover:scale-110 duration-200 cursor-pointer' onClick={() => OnSlider(Books)}><GoCodeReview className='text-2xl' /></a>
+                <a className='mt-8 text-black text-lg bg-white w-16 h-16 rounded-xl text-center p-5 hover:scale-110 duration-200' href="https://book-search-app-react-amber.vercel.app/"><FaLink className='text-2xl' /></a>
+                <a className='mt-8 text-black text-lg bg-white w-16 h-16 rounded-xl text-center p-5 hover:scale-110 duration-200' href="https://github.com/Madanraj0519/Book-search-app-react"><FaGithub className='text-2xl' /></a>
                 </div>
             </div>
+        </div>
 
-            {/* Portfolio */}
-            <div class={`work rounded-lg overflow-hidden border border-zinc-100 p-2 py-24 hover:relative`}>
-                {/* <h1 className='relative -top-16 bg-red-700 w-24 h-8  py-1 px-3 rounded'>Project</h1> */}
-                <img src={portfolio} alt="" className='w-full min-h-full  rounded-lg block '/>
-                <div class={`layer w-full h-0 bg-red-700 rounded-lg ${!isShowMode ? "absolute":""} left-0 bottom-0 overflow-hidden flex items-center justify-center flex-col py-o px-10 text-center text-lg`}>
-                    <h2 className='mb-4 font-semibold'>Portfolio</h2>
-                      <p>Designed and developed a responsive personal portfolio website using React.js and Tailwind CSS.</p>
-                      <p className='hidden md:block'> Created a visually appealing and user-friendly interface to showcase projects, skills, and achievements.</p>
-                    <div className='flex gap-3'>
-                    <a className='mt-8 text-black text-lg bg-white w-16 h-16 rounded-xl text-center p-5 hover:scale-110 duration-200 cursor-pointer' onClick={() => OnSlider(Portfolios)}><GoCodeReview className='text-2xl' /></a>
-                    <a className='mt-8 text-black text-lg bg-white w-16 h-16 rounded-xl text-center p-5 hover:scale-110 duration-200' href="#"><FaLink className='text-2xl' /></a>
-                    <a className='mt-8 text-black text-lg bg-white w-16 h-16 rounded-xl text-center p-5 hover:scale-110 duration-200' href="https://github.com/Madanraj0519/Portfolio-react"><FaGithub className='text-2xl' /></a>
-                    </div>
+        {/* Portfolio */}
+        <div class={`work rounded-lg overflow-hidden border border-zinc-100 p-2 py-24 hover:relative`}>
+            {/* <h1 className='relative -top-16 bg-red-700 w-24 h-8  py-1 px-3 rounded'>Project</h1> */}
+            <img src={portfolio} alt="" className='w-full min-h-full  rounded-lg block '/>
+            <div class={`layer w-full h-0 bg-red-700 rounded-lg ${!isShowMode ? "absolute":""} left-0 bottom-0 overflow-hidden flex items-center justify-center flex-col py-o px-10 text-center text-lg`}>
+                <h2 className='mb-4 font-semibold'>Portfolio</h2>
+                  <p>Designed and developed a responsive personal portfolio website using React.js and Tailwind CSS.</p>
+                  <p className='hidden md:block'> Created a visually appealing and user-friendly interface to showcase projects, skills, and achievements.</p>
+                <div className='flex gap-3'>
+                <a className='mt-8 text-black text-lg bg-white w-16 h-16 rounded-xl text-center p-5 hover:scale-110 duration-200 cursor-pointer' onClick={() => OnSlider(Portfolios)}><GoCodeReview className='text-2xl' /></a>
+                <a className='mt-8 text-black text-lg bg-white w-16 h-16 rounded-xl text-center p-5 hover:scale-110 duration-200' href="#"><FaLink className='text-2xl' /></a>
+                <a className='mt-8 text-black text-lg bg-white w-16 h-16 rounded-xl text-center p-5 hover:scale-110 duration-200' href="https://github.com/Madanraj0519/Portfolio-react"><FaGithub className='text-2xl' /></a>
                 </div>
             </div>
+        </div>
+        </>
+             )
+           }
             
         </div>
         <button class="btn block my-12 mx-auto w-fit border border-red-700 py-4 px-12 rounded-md text-white hover:bg-red-600 " onClick={() => setShowMore(!showMore)}>
