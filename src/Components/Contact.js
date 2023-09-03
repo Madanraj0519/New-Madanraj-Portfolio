c9f5520f27a4012bea2dd65d5eee8e4816ab5f52
Madan raj <125730836+Madanraj0519@users.noreply.github.com>
2023-09-03 14:51:48 +0530

    changes

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -10,7 +10,7 @@ const Contact = () => {
             <span className='text-red-700 uppercase'>Contact</span></h1>
             <div class="row grid grid-cols-1 md:grid-cols-2 gap-5" data-aos="fade-up">
                 <div class="contact-left md:ml-20">
-                    <p className='mt-8 flex items-center text-xl'><FaPaperPlane className='text-red-700 mr-4 text-2xl'/> madan__raj@hot.com</p>
+                    <p className='mt-8 flex items-center text-xl'><FaPaperPlane className='text-red-700 mr-4 text-2xl'/> madanraj0519@gmail.com</p>
                     <p className='mt-8 flex items-center text-xl'><FaPhone className='text-red-700 mr-4 text-2xl' /> 8072441294</p>
                     <p className='mt-8 flex items-center text-xl'><IoLocation className='text-red-700 mr-4 text-2xl' /> Chennai, TamilNadu, India.</p>
                 </div>
